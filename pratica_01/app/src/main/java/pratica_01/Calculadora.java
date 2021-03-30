0d85b84b194b23a34eec55f5a98bdd977b194e68
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2021-03-29 23:19:12 -0300

    Fase Azul

--- pratica_01/app/src/main/java/pratica_01/Calculadora.java
+++ pratica_01/app/src/main/java/pratica_01/Calculadora.java
@@ -2,6 +2,12 @@ package pratica_01;
 
 public class Calculadora {
 
+    /**
+     * Calcula a potencia do primeiro parametro elevado ao segundo parametro
+     * @param x base
+     * @param y expoente
+     * @return x elevado a y
+     */
     public double pow(double x, double y) {
         return Math.pow(x, y);
     }
